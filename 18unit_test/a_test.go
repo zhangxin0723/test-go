5652933caf156c4624e01a1d4cc9ab697e1c3df9
zxin <zhangxin@bjydzy.com>
2026-06-08 17:47:25 +0800

    feat:单元测试

--- 18unit_test/a_test.go
+++ 18unit_test/a_test.go
@@ -3,8 +3,35 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	res := add(1, 2)
-	if res != 3 {
-		t.Errorf("add(1, 2) = %d; want 3", res)
+
+	t.Run("add1", func(t *testing.T) {
+		res := add(1, 2)
+		if res != 3 {
+			t.Errorf("add(1, 2) = %d; want 3", res)
+		}
+	})
+
+	t.Run("add2", func(t *testing.T) {
+		res := add(2, 3)
+		if res != 5 {
+			t.Errorf("add(2, 3) = %d; want 5", res)
+		}
+	})
+
+	cases := []struct {
+		name      string
+		a, b, res int
+	}{
+		{name: "add3", a: 3, b: 4, res: 7},
+		{name: "add4", a: 4, b: 5, res: 9},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			res := add(c.a, c.b)
+			if res != c.res {
+				t.Errorf("add(%d, %d) = %d; want %d", c.a, c.b, res, c.res)
+			}
+		})
 	}
 }
